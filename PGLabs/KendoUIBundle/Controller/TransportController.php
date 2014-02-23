f0316dbe186e6cda75ce0393d69473b909536a61
lmackiewicz <lukasz.mackiewc@leadbullet.pl>
2014-02-23 09:50:21 +0100

    basic

---
+++ PGLabs/KendoUIBundle/Controller/TransportController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PGLabs\KendoUIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class TransportController extends Controller
+{
+    public function transportAction()
+    {
+        
+        $response = new Response();
+        
+        return $response;
+    }
+}
